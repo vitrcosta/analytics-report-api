ef5a3a1ae1d5d9d3a306ac1c772728709f094c78
criativittá <suporte@criativitta.com.br>
2023-08-02 15:06:38 -0300

    add pdf generator def

--- app/controllers/generate_controller.rb
+++ app/controllers/generate_controller.rb
@@ -1,20 +1,18 @@
 class GenerateController < ApplicationController
     def generate_pdf
         pdf = Prawn::Document.new
-        pdf do |pdf|
-            pdf.image "#{Rails.root}/app/assets/images/logomarca-criativitta-menu.png", width: 130, height: 33, position: 405, vposition: 10
-            pdf.text "RELATÓRIO DE ACESSOS - CRIATIVITTÁ"
-            pdf.stroke_horizontal_rule
-            pdf.move_down 20
-            pdf.text "Período: " + params[:start_date] + " até " + params[:end_date]
-            pdf.move_down 20
-            image_data = StringIO.new( Base64.decode64(params[:image]))
-            pdf.image(image_data)
-          end
-          respond_to do |format|
+        pdf.image "#{Rails.root}/app/assets/images/logomarca-criativitta-menu.png", width: 130, height: 33, position: 405, vposition: 10
+        pdf.text "RELATÓRIO DE ACESSOS - CRIATIVITTÁ"
+        pdf.stroke_horizontal_rule
+        pdf.move_down 20
+        pdf.text "Período: " + params[:start_date] + " até " + params[:end_date]
+        pdf.move_down 20
+        image_data = StringIO.new( Base64.decode64(params[:image]))
+        pdf.image(image_data)
+        respond_to do |format|
             format.pdf do
                 send_data pdf.render, filename: 'relatorio.pdf', type: 'application/pdf'
             end
-          end
+        end
     end
 end
