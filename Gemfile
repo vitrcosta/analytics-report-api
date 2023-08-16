76b898fb9075556bd130d05090567e0e16e4dc1e
criativittá <suporte@criativitta.com.br>
2023-08-16 14:21:25 -0300

    gems removed

--- Gemfile
+++ Gemfile
@@ -77,6 +77,7 @@ gem 'google-api-client'
 gem 'matrix'
 gem 'prawn'
 gem 'stringio'
+gem 'dotenv-rails', require: 'dotenv/rails-now', groups: [:development]
 
 
 
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
 module AnalyticsReport
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
